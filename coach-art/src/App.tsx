5a75f56d57642ae18ed42ac823dc6f3c6a418dcc
Tanaya Nawathe <tanayanawathe@Tanayas-MacBook-Air.local>
2024-04-18 11:27:01 -0700

    admin portal landing page

--- coach-art/src/App.tsx
+++ coach-art/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.tsx';
 import AboutPage from './pages/AboutPage.tsx';
 import ReimbursementForm from './pages/ReimbursementForm.tsx';
-
+import AdminLoginPage from './pages/AdminLoginPage.tsx';
+import ResetPasswordPage from './pages/ResetPasswordPage.tsx';
 
 const App: React.FC = () => {
   return (
@@ -12,6 +13,8 @@ const App: React.FC = () => {
         <Route path="/" Component={HomePage} />
         <Route path="/about" Component={AboutPage} />
         <Route path="/reimbursement" Component={ReimbursementForm} />
+        <Route path="/admin" Component={AdminLoginPage} />
+        <Route path="/admin/resetpassword" Component={ResetPasswordPage} />
       </Routes>
     </Router>
   );
